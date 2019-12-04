5b57c272ddce2633863f0f8e5aadeb56bbe1f4da
Romeo STROH <rostroh@e3r5p13.42.fr>
2019-12-04 18:02:25 +0100

    add push pop register asm

--- asm/decode_rc4.s
+++ asm/decode_rc4.s
@@ -33,6 +33,12 @@ decode:
 	push rsi
 	push rdx
 	push rcx
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r15
 
 	lea rdi, [key]
 	lea rsi, [key_size]
@@ -132,6 +138,12 @@ algo:
 		jne .loop
 
 end:
+	pop r15
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
 	pop rcx
 	pop rdx
 	pop rsi
